6d795144847d83629dd30a0e178deb75acc88a05
Unknown <xunpeng715@gmail.com>
2018-04-10 22:54:37 -0400

    change to spider

--- spider.py
+++ spider.py
@@ -1,17 +1,19 @@
 from html.parser import HTMLParser
 from urllib.request import urlopen
 from urllib import parse
+import sys
 
 # We are going to create a class called LinkParser that inherits some
 # methods from HTMLParser which is why it is passed into the definition
 class LinkParser(HTMLParser):
 
-    # This is a function that HTMLParser normally has
+    # This is a function that is inside HTMLParser
     # but we are adding some functionality to it
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for (key, value) in attrs:
                 if key == 'href':
+                    # join root link and abstract link
                     newUrl = parse.urljoin(self.baseUrl, value)
                     self.links = self.links + [newUrl]
 
@@ -19,47 +21,36 @@ class LinkParser(HTMLParser):
     # that our spider() function will call
     def getLinks(self, url):
         self.links = []
-        # Remember the base URL which will be important when creating
-        # absolute URLs
         self.baseUrl = url
-        # Use the urlopen function from the standard Python 3 library
         response = urlopen(url)
         if response.getheader('Content-Type')=='text/html':
             htmlBytes = response.read()
-            # Note that feed() handles Strings well, but not bytes
-            # (A change from Python 2.x to Python 3.x)
             htmlString = htmlBytes.decode("utf-8")
             self.feed(htmlString)
-            return htmlString, self.links
+            return self.links
         else:
-            return "",[]
+            return []
 
-# And finally here is our spider. It takes in an URL, a word to find,
-# and the number of pages to search through before giving up
-def spider(url, word, maxPages):
+# using BFS to
+def spider(url, maxPages = sys.maxsize):
     pagesToVisit = [url]
     numberVisited = 0
-    foundWord = False
-    while numberVisited < maxPages and pagesToVisit != [] and not foundWord:
+    visitedlinks = set()
+    while numberVisited < maxPages and pagesToVisit != []:
         numberVisited = numberVisited +1
         url = pagesToVisit[0]
         pagesToVisit = pagesToVisit[1:]
-
+        visitedlinks.add(url)
         try:
             print(numberVisited, "Visiting:", url)
             parser = LinkParser()
-            data, links = parser.getLinks(url)
-            print("links: " + links)
-            if data.find(word)>-1:
-                foundWord = True
-                pagesToVisit = pagesToVisit + links
-                print(" **Success!**")
+            links = parser.getLinks(url)
+            pagesToVisit = pagesToVisit + links
+            # print(pagesToVisit)
+            print(" **Success!**")
         except:
             print(" **Failed!**")
-    if foundWord:
-        print("The word", word, "was found at", url)
-    else:
-        print("Word never found")
+    return visitedlinks
 
-
-spider("https://www.dreamhost.com", "secure", 20)
+links = spider("https://www.dreamhost.com", 22)
+print(links)
--- spider_v2.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import re
-
-"""Scan the text for http URLs and return a set
-    of URLs found, without duplicates"""
-html_page = urllib.request.urlopen("http://www.python.org/")
-plain_text = html_page.read()
-soup = BeautifulSoup(plain_text, "lxml")
